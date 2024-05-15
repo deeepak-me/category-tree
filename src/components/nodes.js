a92738c1978fd13b1773a0ddff6730cb9a6d5402
deeepak-me <deepak.krishnan.e@gmail.com>
2024-05-16 01:44:28 +0530

    feat: added dropdown feature and icons

--- src/components/nodes.js
+++ src/components/nodes.js
@@ -1,29 +1,53 @@
-import React from "react";
-import styles from "./nodes.module.css";
+"use client";
+
+import React, { useState } from "react";
+import "./nodes.css";
 import Link from "next/link";
+import { FaAngleDown, FaAngleRight, FaCircle } from "react-icons/fa";
 
 const Node = {};
 
 const Nodes = ({ categories }) => {
+  const [openCategories, setOpenCategories] = useState([]);
+
+  const toggleCategory = (key) => {
+    if (openCategories.includes(key)) {
+      setOpenCategories(openCategories.filter((item) => item !== key));
+    } else {
+      setOpenCategories([...openCategories, key]);
+    }
+  };
   return (
-    // <ul className="nodes">
-    //   {nodes.map((node) => {
-    //     // return <Node key={node.id} {...node} />;
-    //     <div>
-    //       <li>
-    //         <a>computers</a>
-    //       </li>
-    //       ;
-    //     </div>;
-    //   })}
-    // </ul>
-    <ul>
+    <ul className="nodes">
       {categories.map((category) => (
         <li key={category.key}>
-          <Link href={category.url}>{category.name}</Link>
-          {category.categories && category.categories.length > 0 && (
-            <Nodes categories={category.categories} />
-          )}
+          <div className="category-container">
+            <div className="category-item">
+              {category.categories ? (
+                openCategories.includes(category.key) ? (
+                  <FaAngleDown className="category-icon arrow-down" />
+                ) : (
+                  <FaAngleRight className="category-icon arrow-icon" />
+                )
+              ) : (
+                <FaCircle className="category-icon circle-icon" />
+              )}
+              <Link
+                href={category.url}
+                onClick={() => toggleCategory(category.key)}
+                className="name"
+              >
+                {category.name}
+              </Link>
+            </div>
+            {category.categories &&
+              openCategories.includes(category.key) &&
+              category.categories.length > 0 && (
+                <div className="subcategory-container">
+                  <Nodes categories={category.categories} />
+                </div>
+              )}
+          </div>
         </li>
       ))}
     </ul>
